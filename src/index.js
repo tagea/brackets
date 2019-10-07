9b2fe18f98f0acbe63d5d9d24e4343165829dcbe
Yulya Vasilyeva <043@tut.by>
2019-10-07 18:03:02 +0300

    feat: create task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strCopy = str;
+  let iter = 0;
+  if(strCopy.length % 2 != 0 ){
+    return false;
+  }else{
+    while(iter <= bracketsConfig.length && strCopy.length){
+      iter++;
+      bracketsConfig.forEach(item => {
+        const concatBr = item[0] + item[1];
+      
+        let equalBr = strCopy.indexOf(concatBr);
+        while(equalBr !== -1){
+          // delete found brackets
+          strCopy = strCopy.substring(0, equalBr) + strCopy.substring(equalBr + concatBr.length, strCopy.length);
+          equalBr = strCopy.indexOf(concatBr);
+        }    
+      });
+    }
+
+    if(strCopy.length){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
 }
